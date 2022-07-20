ae4d4394e125025f5bde6c7544448f069c0a68eb
DanieleDifra <daniele.df98@gmail.com>
2022-07-20 13:12:16 +0200

    first commit

--- telegramBot.py
+++ telegramBot.py
@@ -12,39 +12,37 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 # set up GPIO output channel
 GPIO.setup(11, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 
 updater = Updater(token="5434499546:AAE6TfxPDbKsX4ajVIFFcqWQUmIf3RpOt4Q")
 dispatcher = updater.dispatcher
 
-ledOn = "LED on"
-ledOff = "LED off"
+pot11 = "Water pot 11"
+pot16 = "Water pot 16"
 
 allowedUsernames = ["DanieleDifra"]
 
 def startCommand(update: Update, context: CallbackContext):
-    buttons = [[KeyboardButton(ledOn)], [KeyboardButton(ledOff)]]
+    buttons = [[KeyboardButton(pot11)], [KeyboardButton(pot16)]]
     context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my bot!", reply_markup=ReplyKeyboardMarkup(buttons))
 
 def messageHandler(update: Update, context: CallbackContext):
     if update.effective_chat.username not in allowedUsernames:
         context.bot.send_message(chat_id=update.effective_chat.id, text="You are not allowed to use this bot")
         return
-    if ledOn in update.message.text:
+    if pot11 in update.message.text:
         GPIO.output(11,True)
-        context.bot.send_message(chat_id=update.effective_chat.id, text="LED turned on", reply_markup=ReplyKeyboardMarkup(buttons))
-    if ledOff in update.message.text:
-        GPIO.output(11, False)
-        context.bot.send_message(chat_id=update.effective_chat.id, text="LED turned off", reply_markup=ReplyKeyboardMarkup(buttons))
-    if randomPeopleText in update.message.text:
-        image = get(randomPeopleUrl).content
-    if randomImageText in update.message.text:
-        image = get(randomPImageUrl).content
-
-    if image:
-        context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto(image, caption="")])
-
-        buttons = [[InlineKeyboardButton("👍", callback_data="like")], [InlineKeyboardButton("👎", callback_data="dislike")]]
-        context.bot.send_message(chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(buttons), text="Did you like the image?")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Watering pot 11", reply_markup=ReplyKeyboardMarkup(buttons))
+        time.sleep(5)
+        GPIO.output(11,False)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Done", reply_markup=ReplyKeyboardMarkup(buttons))
+        
+    if pot16 in update.message.text:
+        GPIO.output(16,True)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Watering pot 16", reply_markup=ReplyKeyboardMarkup(buttons))
+        time.sleep(5)
+        GPIO.output(16,False)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Done", reply_markup=ReplyKeyboardMarkup(buttons))
 
 dispatcher.add_handler(CommandHandler("start", startCommand))
 dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))
