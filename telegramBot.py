9765aa9f88ff6df9f2b3438f631e67d543940c60
DanieleDifra <daniele.df98@gmail.com>
2022-07-20 16:41:07 +0200

    response fix

--- telegramBot.py
+++ telegramBot.py
@@ -63,5 +63,6 @@ updater.start_polling()
 def getWeather():
     weather_url = "http://dataservice.accuweather.com/currentconditions/v1/214046?apikey=GoxexX06khkOOTkiUFNfFB0Lh0tnAo1"
     response = requests.get(url)
-    return response = json.loads(response)
+    json_response = json.loads(response)
+    return json_response
      
