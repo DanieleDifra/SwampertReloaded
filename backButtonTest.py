894354bc48fed735329e8357cd3dd929d24c0765
DanieleDifra <daniele.df98@gmail.com>
2022-07-22 16:36:45 +0200

    another fix

--- backButtonTest.py
+++ backButtonTest.py
@@ -320,39 +320,16 @@ def main() -> None:
     # Create the Application and pass it your bot's token.
     application = Application.builder().token("5434499546:AAE6TfxPDbKsX4ajVIFFcqWQUmIf3RpOt4Q").build()
 
-    # Set up third level ConversationHandler (collecting features)
-    description_conv = ConversationHandler(
-        entry_points=[
-            CallbackQueryHandler(
-                select_feature, pattern="^" + str(MALE) + "$|^" + str(FEMALE) + "$"
-            )
-        ],
-        states={
-            SELECTING_FEATURE: [
-                CallbackQueryHandler(ask_for_input, pattern="^(?!" + str(END) + ").*$")
-            ],
-            TYPING: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_input)],
-        },
-        fallbacks=[
-            CallbackQueryHandler(end_describing, pattern="^" + str(END) + "$"),
-            CommandHandler("stop", stop_nested),
-        ],
-        map_to_parent={
-            # Return to second level menu
-            END: SELECTING_LEVEL,
-            # End conversation altogether
-            STOPPING: STOPPING,
-        },
-    )
-
     # Set up second level ConversationHandler (adding a person)
     add_member_conv = ConversationHandler(
         entry_points=[CallbackQueryHandler(select_pot, pattern="^" + str(WATER_POTS) + "$")],
         states={
             SELECTING_LEVEL: [
-                CallbackQueryHandler(select_gender, pattern=f"^{PARENTS}$|^{CHILDREN}$")
-            ],
-            SELECTING_GENDER: [description_conv],
+                CallbackQueryHandler(pot1, pattern="^" + str(POT1) + "$"),
+                CallbackQueryHandler(pot2, pattern="^" + str(POT2) + "$"),
+                CallbackQueryHandler(pot3, pattern="^" + str(POT3) + "$"),
+                CallbackQueryHandler(everyPot, pattern="^" + str(EVERY) + "$"),
+            ]
         },
         fallbacks=[
             CallbackQueryHandler(weather, pattern="^" + str(INFO) + "$"),
@@ -370,17 +347,10 @@ def main() -> None:
     )
 
     # Set up top level ConversationHandler (selecting action)
-    # Because the states of the third level conversation map to the ones of the second level
-    # conversation, we need to make sure the top level conversation can also handle them
     selection_handlers = [
-        add_member_conv,
         CallbackQueryHandler(info, pattern="^" + str(INFO) + "$"),
         CallbackQueryHandler(weather, pattern="^" + str(WEATHER) + "$"),
-        CallbackQueryHandler(end, pattern="^" + str(END) + "$"),
-        CallbackQueryHandler(pot1, pattern="^" + str(POT1) + "$"),
-        CallbackQueryHandler(pot2, pattern="^" + str(POT2) + "$"),
-        CallbackQueryHandler(pot3, pattern="^" + str(POT3) + "$"),
-        CallbackQueryHandler(everyPot, pattern="^" + str(EVERY) + "$"),
+        CallbackQueryHandler(end, pattern="^" + str(END) + "$")
     ]
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
@@ -389,7 +359,6 @@ def main() -> None:
             WEATHER: [CallbackQueryHandler(start, pattern="^" + str(END) + "$")],
             SELECTING_ACTION: selection_handlers,
             SELECTING_LEVEL: selection_handlers,
-            DESCRIBING_SELF: [description_conv],
             STOPPING: [CommandHandler("start", start)],
         },
         fallbacks=[CommandHandler("stop", stop)],
