30c4d67fbff35893c934e022c39d573f852edebb
DanieleDifra <daniele.df98@gmail.com>
2022-07-22 13:43:37 +0200

    dependancies

--- backButtonTest.py
+++ backButtonTest.py
@@ -1,14 +1,8 @@
 """
-First, a few callback functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Example of a bot-user conversation using nested ConversationHandlers.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
+
 """
 
+## Dependancies
 import logging
 from typing import Any, Dict, Tuple
 
@@ -35,6 +29,15 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
+
+import requests
+import json
+
+import paho.mqtt.publish as publish
+import psutil
+import string
+
+import RPi.GPIO as GPIO
 
 # Enable logging
 logging.basicConfig(
