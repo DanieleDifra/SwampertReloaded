392080fe6f0ada2c38da71b8406fd5b4f54df5c6
DanieleDifra <you@example.com>
2022-07-25 19:40:10 +0200

    variabilization

--- misc/testVar.py
+++ misc/testVar.py
@@ -1,7 +1,11 @@
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+from dotenv import load_dotenv, find_dotenv
+import os
+### give the full path
+path='/home/swampi/SwampertReloaded/misc/sample.env'
+
+load_dotenv(dotenv_path=path,verbose=True)
 
 tg_token = os.getenv("TELEGRAMTOKEN")
-print(tg_token)
+print(str(tg_token))
+
