122a280d9d02b2f268cc2054761e93ce65c4e611
DanieleDifra <you@example.com>
2022-07-25 14:58:46 +0200

    various fix

---
+++ runScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ps -C python >/dev/null; then
+    echo "TelegramBot is already running."
+else
+    echo "Booting TelegramBot"
+    python /home/swampi/SwampertReloaded/telegramBot.py >> botLogs.txt
+fi
