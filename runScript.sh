99eb6afd66352b2922f44408463787d95f9bc44f
DanieleDifra <you@example.com>
2022-08-06 08:35:06 +0200

    [FIX] Edited running script logging

--- runScript.sh
+++ runScript.sh
@@ -4,7 +4,6 @@ if ps -C python >/dev/null; then
     echo "TelegramBot is already running."
 else
     echo "Booting TelegramBot"
-    sleep 5m
     python /home/swampi/SwampertReloaded/telegramBot.py 
 fi
 
