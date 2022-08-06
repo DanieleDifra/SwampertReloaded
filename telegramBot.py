b516329fed3680352637bc69f6d290a790ab4656
DanieleDifra <you@example.com>
2022-08-06 08:42:04 +0200

    [FIX] Removed log file

--- telegramBot.py
+++ telegramBot.py
@@ -78,7 +78,7 @@ topic = "channels/" + channel_ID + "/publish"
 
 ## Telegram code
 # Enable logging
-logging.basicConfig(filename="botLog.log", format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
